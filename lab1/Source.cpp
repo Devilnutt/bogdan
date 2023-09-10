ef47610f1bcd1ccc95c71288e5140b9d438e5d9c
Bogdan <yurev.bogdan.2005@mail.ru>
2023-09-10 23:02:28 +0300

    Create Source.cpp

---
+++ lab1/Source.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main() {
+	double a, b, c, Answer1, Answer2, D;
+	std::cout << "A:" && std::cin >> a && std::cout << "B:" && std::cin >> b && std::cout << "C:" && std::cin >> c;
+	D = pow(b, 2) - 4 * a * c;
+	Answer1 = (-1 * b + sqrt(D)) / (2 * a);
+	Answer2 = (-1 * b - sqrt(D)) / (2 * a);
+	if (D > 0) {
+		std::cout << "Answer: " << Answer1 << ";" << Answer2;
+	}
+	else if (D < 0) {
+		std::cout << "No answers in R";
+	}
+	else if (D == 0) {
+		std::cout << "Answer: " << Answer1;
+	}
+	return 0;
+}
